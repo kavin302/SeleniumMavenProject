e314b5d5b4d33d59e9342223ed3c3b0a1de55a2f
kavin302 <123706517+kavin302@users.noreply.github.com>
2023-04-05 20:54:14 +0530

    Day02

--- src/test/java/day02/Project.java
+++ src/test/java/day02/Project.java
@@ -1,1 +1,17 @@
 package day02;
+public class Project extends Framework{
+
+	public static void main(String[] args) {
+		startReport("demoapplication");
+		startTest("demo", "testing application");
+		browser("edge");
+		appUrl("https://accounts.google.com/signup");
+		typeIn("//input[@name='firstName']", "testA","firstName");
+		typeIn("//input[@name='lastName']", "testA","lastName");
+		clickIn("//input[@type='checkbox']","showPassword");
+		clearAll("//input[@name='firstName']","firstName");
+		endTest();
+		endReport();
+	}
+
+}
